23f871406bca77620846a7625e51031ae7852348
KrystalY <yoosj0703@gmail.com>
2020-10-28 20:58:58 +0900

    update files

---
+++ Playground/TypeScript/src/app.ts
@@ -0,0 +1,7 @@
+console.log('test');
+
+const btn = document.querySelector("button")!;
+
+btn.addEventListener("click", () => {
+	console.log("clicked");
+});
--- Playground/TypeScript/src/basics.ts
+++ Playground/TypeScript/src/basics.ts
@@ -1,6 +1,6 @@
 // JS uses "dynamic types" (resolved at runtime)
 // TS uses "static types" (set during development)
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add01(n1: number, n2: number, showResult: boolean, phrase: string) {
 	// if (typeof n1 !== 'number' || typeof n2 !== 'number') {
 	// 	throw new Error('Incorrect input!');
 	// }
@@ -17,4 +17,4 @@ const number2 = 2.6;
 const printResult = true;
 const resultPhrase = 'Result is: ';
 
-add(number1, number2, printResult, resultPhrase);
+add01(number1, number2, printResult, resultPhrase);
